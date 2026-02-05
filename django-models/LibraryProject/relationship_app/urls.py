65cd13a1000bc9f21de40c1b8cc983b34e88daaf
karigosammy5 <karigosammy5@gmail.com>
2026-02-05 04:33:40 +0300

    Restore and update URLs for book permissions

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
+
+    # URLs for views with custom permissions
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 ]
