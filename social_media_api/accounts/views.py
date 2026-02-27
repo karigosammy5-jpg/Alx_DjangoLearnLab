27af6233b9423d483bde98961b619e3cdba13d46
karigosammy5 <karigosammy5@gmail.com>
2026-02-27 20:55:43 +0300

    updated urls.py file

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
@@ -17,7 +17,7 @@ class LoginView(ObtainAuthToken):
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key, 'user_id': user.pk, 'email' : user.email}, status= status.HTTP_200_OK)
+        return Response({'token': token.key, 'user_id': user.pk, 'email': user.email}, status=status.HTTP_200_OK)
 
 
 class ProfileView(generics.RetrieveUpdateAPIView):
